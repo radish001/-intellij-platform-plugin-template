93b036602dc43d593686b4c25db77ac94a95fdaa
GitHub Action <action@github.com>
2022-03-02 10:34:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/radish001/intellijplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.radish001.intellijplatformplugintemplate.services
+
+import com.github.radish001.intellijplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/radish001/intellijplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/radish001/intellijplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.radish001.intellijplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.radish001.intellijplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
